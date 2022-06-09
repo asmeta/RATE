d7525a5b3cd207a955cad84858a826cf12f01298
a-bombarda <andrea.bombarda@unibg.it>
2022-06-09 11:48:47 +0200

    File rename

--- Case_studies/MVM/TestExecutor/src/scenarioconverter/test/Test.java
+++ Case_studies/MVM/TestExecutor/src/scenarioconverter/test/Test.java
@@ -74,8 +74,24 @@ public class Test {
 			add(new Configuration("3", "config/config3.json", CriteriaEnum.BASIC_RULE, CriteriaEnum.COMBINATORIAL_MON, CriteriaEnum.COMPLETE_RULE, CriteriaEnum.MCDC, CriteriaEnum.RULE_GUARD, CriteriaEnum.RULE_UPDATE, CriteriaEnum.THREEWISE_MON));
 			add(new Configuration("NR", "config/config3.json", CriteriaEnum.BASIC_RULE, CriteriaEnum.COMBINATORIAL_MON, CriteriaEnum.COMPLETE_RULE, CriteriaEnum.MCDC, CriteriaEnum.RULE_GUARD, CriteriaEnum.RULE_UPDATE, CriteriaEnum.THREEWISE_MON));
 			add(new Configuration("00", "config/config3.json", CriteriaEnum.BASIC_RULE, CriteriaEnum.COMBINATORIAL_MON, CriteriaEnum.COMPLETE_RULE, CriteriaEnum.MCDC, CriteriaEnum.RULE_GUARD, CriteriaEnum.RULE_UPDATE, CriteriaEnum.THREEWISE_MON));
-			add(new Configuration("01", "config/config3.json", /*CriteriaEnum.BASIC_RULE, CriteriaEnum.COMBINATORIAL_MON, CriteriaEnum.COMPLETE_RULE, CriteriaEnum.MCDC, CriteriaEnum.RULE_GUARD, CriteriaEnum.RULE_UPDATE,*/ CriteriaEnum.THREEWISE_MON));
-			add(new Configuration("02", "config/config3.json", CriteriaEnum.BASIC_RULE, CriteriaEnum.COMBINATORIAL_MON, CriteriaEnum.COMPLETE_RULE, CriteriaEnum.MCDC, CriteriaEnum.RULE_GUARD, CriteriaEnum.RULE_UPDATE, CriteriaEnum.THREEWISE_MON));
+			add(new Configuration("01", "config/config3.json"
+					, CriteriaEnum.BASIC_RULE
+					, CriteriaEnum.COMBINATORIAL_MON
+					, CriteriaEnum.COMPLETE_RULE
+					, CriteriaEnum.MCDC
+					, CriteriaEnum.RULE_GUARD
+					, CriteriaEnum.RULE_UPDATE
+					, CriteriaEnum.THREEWISE_MON
+					));
+			add(new Configuration("02", "config/config3.json"
+//					,CriteriaEnum.BASIC_RULE
+//					,CriteriaEnum.COMBINATORIAL_MON
+					,CriteriaEnum.COMPLETE_RULE
+//					,CriteriaEnum.MCDC
+//					,CriteriaEnum.RULE_GUARD
+//					,CriteriaEnum.RULE_UPDATE
+//					,CriteriaEnum.THREEWISE_MON
+					));
 			
 		}
 	};	
@@ -86,7 +102,7 @@ public class Test {
 				"statechart", "MAIN_REGION__final_", out);
 		
 		// XXX Select the configuration you want to execute
-		Configuration c = filesToProcess.get(41);
+		Configuration c = filesToProcess.get(42);
 		
 		try {
 			ScenarioConverter.CONFIG_PATH = c.configPath;

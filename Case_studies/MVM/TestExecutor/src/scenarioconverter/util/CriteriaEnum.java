17606c350030d0b207357ac055e1dfe16f777d69
Angelo Gargantini <angelo.gargantini@unibg.it>
2022-06-29 14:29:04 +0200

    mutation

---
+++ Case_studies/MVM/TestExecutor/src/scenarioconverter/util/CriteriaEnum.java
@@ -0,0 +1,27 @@
+package scenarioconverter.util;
+
+public enum CriteriaEnum{
+	BASIC_RULE("BasicRule","BR"), 
+	COMPLETE_RULE("CompleteRule","CR"),
+	RULE_GUARD("RuleGuard","RG"),
+	RULE_UPDATE("RuleUpdate","RU"),
+	MCDC("MCDC","MCDC"),
+	// combinatorial
+	COMBINATORIAL_MON("pairwise monitored", "pair"),
+	COMBINATORIAL_ALL("pairwise all", "pair"),
+	
+	THREEWISE_ALL("3wise", "3-wise"),
+	THREEWISE_MON("3wise", "3-wise");
+	
+	
+	private String prefix;
+
+	CriteriaEnum(String name, String prefix) {
+		this.prefix = prefix;
+	}
+
+	public String getAbbrvName() {
+		return prefix;
+	}
+	
+}

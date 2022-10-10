e5b43dfde9c0f58ee0008f44cfa8065949963422
Angelo Gargantini <angelo.gargantini@unibg.it>
2022-10-10 13:55:04 +0200

    new test

--- Case_studies/MVM/TestExecutor/src/scenarioconverter/test/CompileAndExecuteTest.java
+++ Case_studies/MVM/TestExecutor/src/scenarioconverter/test/CompileAndExecuteTest.java
@@ -22,9 +22,9 @@ public class CompileAndExecuteTest {
 	private static boolean WRITE_JSON = false;
 
 	public static List<Configuration> filesToProcess = new ArrayList<Configuration>() {
-		
+
 		private static final long serialVersionUID = 5014712752302397500L;
-		{		
+		{
 			// level 0
 //			add(new Configuration("00", "config/config3.json", Configuration.MANUAL_TEST));
 //			add(new Configuration("00", "config/config3.json", Configuration.ALL_CRITERIA));
@@ -41,29 +41,31 @@ public class CompileAndExecuteTest {
 //			add(new Configuration("NR", "config/config3.json", Configuration.MANUAL_TEST));
 //			add(new Configuration("NR", "config/config3.json", Configuration.ALL_CRITERIA));
 		}
-	};	
+	};
+	
 	
 	public static void main(String[] args) throws Exception {
 		// Now compile the MVM code with the GoogleTests
 		// executeBat("build_gtest.bat");
 		// executeBat("build_MVM.bat");
 		// XXX Select the configuration you want to execute
-		//Configuration c = filesToProcess.get(0);
+		// Configuration c = filesToProcess.get(0);
 		List<Boolean> failures = new ArrayList<>();
-		for(Configuration c: filesToProcess) {
+		for (Configuration c : filesToProcess) {
 			failures.add(executeTest(c));
-			//break;
+			// break;
 		}
 		System.out.println(failures);
 	}
 
-	static boolean executeTest(Configuration c) throws FileNotFoundException, Exception, IOException, InterruptedException {
-		
+	static boolean executeTest(Configuration c)
+			throws FileNotFoundException, Exception, IOException, InterruptedException {
+
 		// The path of abstract tests
 		String PATH_AT;
-		
+
 		PrintWriter out = new PrintWriter(new File(DESTINATION_PATH + "test.cpp"));
-		
+
 		ScenarioConverter sc = new ScenarioConverter("state", "MVMStateMachineCore", "MVMStateMachineCoreStates",
 				"statechart", "MAIN_REGION__final_", out);
 		try {
@@ -71,14 +73,17 @@ public class CompileAndExecuteTest {
 			if (c.criteria == Configuration.MANUAL_TEST) {
 				// Manual scenarios
 				PATH_AT = "../ASM/MVM_" + c.level + "/scenarios/";
-				
+
 				// Prints the includes and the mock instructions
 				sc.printIncludes();
 				sc.printMock("./additional_files/mock_simple.c");
 
 				assert new File(PATH_AT).exists();
 
-				Files.walk(new File(PATH_AT).toPath()).filter(f -> (f.getFileName().toString().endsWith(".avalla")))
+				Files.walk(new File(PATH_AT).toPath()).filter(f -> (
+						f.getFileName().toString().endsWith(".avalla"))
+						&& f.getFileName().toString().endsWith("12.avalla")
+						)
 						.forEach(f -> {
 							try {
 								sc.convert(f.toAbsolutePath().toString());
@@ -109,7 +114,7 @@ public class CompileAndExecuteTest {
 			e.printStackTrace();
 		}
 		out.close();
-		
+
 //		boolean failed = executeBat("build_and_exec_test.bat");
 		boolean failed = executeBat("commands_for_building_MVM.bat");
 		System.err.println("+++ " + failed);
@@ -119,20 +124,20 @@ public class CompileAndExecuteTest {
 	static boolean executeBat(String batfile) {
 		System.out.println("executing bath file " + batfile);
 		ProcessBuilder builder = new ProcessBuilder();
-		builder.command("cmd.exe", "/c",
-				new File("./additional_files/"+batfile).getAbsolutePath());
+		builder.command("cmd.exe", "/c", new File("./additional_files/" + batfile).getAbsolutePath());
 		builder.directory(new File(DEBUG_PATH));
 		// comment this to have a separate IO for this process
-		//builder.inheritIO();
+		// builder.inheritIO();
 		boolean failed = false;
 		try {
 			Process process = builder.start();
 			BufferedReader buf = new BufferedReader(new InputStreamReader(process.getInputStream()));
-			String line = "";		
+			String line = "";
 			while ((line = buf.readLine()) != null) {
 				System.out.println(line);
-				// [  FAILED  ]
-				if (line.startsWith("[  FAILED  ]")) failed = true;
+				// [ FAILED ]
+				if (line.startsWith("[  FAILED  ]"))
+					failed = true;
 			}
 			process.waitFor();
 		} catch (IOException e) {
@@ -141,7 +146,7 @@ public class CompileAndExecuteTest {
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}		
+		}
 		return failed;
 	}
 }
